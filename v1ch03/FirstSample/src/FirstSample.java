03e23c0dc43049e523f4f4ddf11f4a7c995b1b91
changxing <1278729001@qq.com>
2023-10-24 08:40:40 +0800

    新增:程序清单3-1

---
+++ v1ch03/FirstSample/src/FirstSample.java
@@ -0,0 +1,12 @@
+/**
+ * This is the first sample program in Core Java Chapter 3
+ *
+ * @author Gary Cornell
+ * @version 1.01 1997-03-22
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        // 使用 System.out 对象并调用了它的 println 方法，将一个文本行输出到控制台上
+        System.out.println("We will not use 'Hello, world!'");
+    }
+}
